6c428b8430976573a07328f979636d46a28a524a
shadow2505 <141756242+shadow2505@users.noreply.github.com>
2023-08-15 10:29:59 -0600

    Add files via upload

---
+++ genraciones de computadoras.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+int main(int argc, char **argv )
+{
+	printf("\033[1m                                       ______________________________\033[0m\n");
+	printf("\033[1m                                       |GENERACIONES DE COMPUTADORAS|\033[0m\n");
+	printf("\033[1m                                 ****************************************** \033[0m\n\n");
+    printf("                                  Primera Generacion (decadas de 1940-1950): \n");
+	printf("                                              -Valvulas de vacio.              \n");
+	printf("                                    -Grandes y ocupaban habitaciones enteras. \n");
+	printf("                                      -Programacion en lenguaje de maquina    \n");
+	printf("\033[1m                                 ******************************************\033[0m\n\n");
+	printf("                                  Segunda Generacion (decadas de 1950-1960):\n");
+	printf("                                               -transistores \n");
+	printf("                                    -Tecnologia de montaje en placa (PCB). \n");
+	printf("                               -Uso de lenguajes de programacion de alto nivel.\n");
+	printf("\033[1m                                  ******************************************\033[0m\n\n");
+	printf("                                  Tercera Generacion (decadas de 1960-1970) : \n");
+	printf("                                           -Circuitos Integrados. \n ");
+	printf("                                       -aparicion de minicomputadoras. \n");
+	printf("                                 -Uso de sistemas operativos mas avanzados.\n");
+	printf("\033[1m                                  ******************************************\033[0m\n\n");
+	printf("                                  Cuarta Generacion (decadas de 1970-1980) :\n");
+	printf("                                            -Microprocesadores. \n");
+	printf("                                    -Desarrollo de equipos personales (PC). \n");
+	printf("                               -Avances en graficos y sistemas de almacenamiento.\n");
+	printf("\033[1m                                 ******************************************\033[0m\n\n");
+	printf("                                  Quinta Generacion (1980-Presente) :\n");
+	printf("                                 -Avances en Inteligencia Artificial. \n");
+	printf("                                -Computadores mas rapidos y pequeños. \n");
+	printf("                              -Interconexion global a traves de Internet.\n");
+	printf("\033[1m                                 ******************************************\033[0m\n\n");
+return 0;
+}
+
